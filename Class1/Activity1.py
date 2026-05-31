41febab031736a323ec3a4be9a9891dcc314f1c4
Harsha <swethachalu@gmail.com>
2026-05-31 11:46:04 +0100

    classwork

--- Class1/Activity1.py
+++ Class1/Activity1.py
@@ -3,5 +3,5 @@ print("Hello World")
 #printing my name
 print("My name is Harsha")
 
-#performing simple calculation
+#\performing simple calculation
 print("2+2=",2+2)
---
+++ Class1/classwork-1.py
@@ -0,0 +1,10 @@
+print("Hi, my name is Harsha")
+
+#My school name and grade
+print("I am from Harlaw academy and I am S1")
+
+#My age details
+print("Im 12yrs old")
+
+#Subjects at classes
+print("I study Maths,English,design and technology,ICT,Bussiness and education,science,R.M.E,Social subjects and German.")
