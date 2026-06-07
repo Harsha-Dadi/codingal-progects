7effb379eab630ae3c9573b7db980bdfa7f50fd5
Harsha <swethachalu@gmail.com>
2026-06-07 11:33:19 +0100

    routine_classwork

---
+++ Class 2/my_daily_routine_printer/identifierofoddoreven.py
@@ -0,0 +1,5 @@
+number=input("enter your number")
+
+print("given number is even",number/2:)
+else
+print("given number is odd")
---
+++ Class 2/my_daily_routine_printer/routine.py
@@ -0,0 +1,7 @@
+#My daily routine
+print("My Daily Routine")
+
+#When i wake up
+print("I wake up at 7AM\nI go to school at 8 AM\nI play with my friends in the evening\nI study 2 hours")
+
+print("Total study hours in a week:",2*7)
