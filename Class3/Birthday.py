04e20388bbf043bfc3eb801c1bc65666c6922042
Harsha <swethachalu@gmail.com>
2026-06-28 12:19:35 +0100

    Birthdays.py

---
+++ Class3/Birthday.py
@@ -0,0 +1,11 @@
+friend1="2/2/13"
+friend2="1/5/13"
+friend3="25/6/13"
+friend4="19/10/14"
+friend5="31/12/14"
+
+print("Rishi's birthday is at",friend1)
+print("Ranish's birthday is at",friend2)
+print("Tishi's birthday is at",friend3)
+print("Rihi's birthday is at",friend4)
+print("shiva's birthday is at",friend5)
